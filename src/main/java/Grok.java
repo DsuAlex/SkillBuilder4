ca677e26a400d4f58bf294066c0a40d0a36b9be4
alexedwards <agedwards23@students.desu.edu>
2024-02-25 22:28:23 -0500

    good job!

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -6,82 +6,58 @@
  * @author (You Again)
  * @version (0.1)
  */
-public class Grok
-{
-    private static final int DEFAULT_POWER_LEVEL=50;
+
+public class Grok {
+    private static final int DEFAULT_POWER_LEVEL = 50;
     private static final int MAX_POWER_LEVEL = 100;
 
-    // instance variables
     private int powerLevel;
     private boolean isAlive;
 
-    /*
-     * Initializes a Grok object to the default power level of 50.
-     */
-    public Grok()
-    {
+    public Grok() {
         setPowerLevel(DEFAULT_POWER_LEVEL);
     }
 
-    /*
-     * Initializes a Grok object to power powerLevel
-     *
-     * @param powerLevel power level of this Grok
-     */
-    public Grok(int powerLevel)
-    {
+    public Grok(int powerLevel) {
         setPowerLevel(powerLevel);
     }
 
-    // accessor methods
-
-    /*
-     * Returns the power level of this Grok.
-     * @return returns the power level of this Grok
-     */
-    public int getPowerLevel()
-    {
+    public int getPowerLevel() {
         return powerLevel;
     }
 
-    public boolean isDead()
-    {
-        // TODO: replace this line with your code here
+    public boolean isDead() {
+        return !isAlive || powerLevel <= 0;
     }
 
-    // mutator methods
-
-    /*
-     * Sets the power level of this Grok.
-     * @param powerLevel the power value to set for this Grok.
-     */
-    public void setPowerLevel(int powerLevel)
-    {
-        this.powerLevel = powerLevel;
+    public void setPowerLevel(int powerLevel) {
+        if (powerLevel <= 0) {
+            this.powerLevel = 0;
+            isAlive = false;
+        } else if (powerLevel > MAX_POWER_LEVEL) {
+            this.powerLevel = MAX_POWER_LEVEL;
+            isAlive = true;
+        } else {
+            this.powerLevel = powerLevel;
+            isAlive = true;
+        }
     }
 
-    /*
-     * Set the power level of this Grok to the power level of
-     * the pill.
-     * @param pill The PowerPill that the this Grok.  The power
-     * of the pill is added to the power level of this Grok.
-     */
-    public void takePowerPill(PowerPill pill)
-    {
-        setPowerLevel(powerLevel + pill.getPower());
+    public void takePowerPill(PowerPill pill) {
+        if (isAlive) {
+            setPowerLevel(Math.min(powerLevel + pill.getPower(), MAX_POWER_LEVEL));
+        }
     }
 
-    /*
-     * Invoked when this Grok takes a hit.  The power level of
-     * this Grok is reduced by 5.
-     */
-    public void tookHit()
-    {
-        setPowerLevel(powerLevel - 5);
+    public void tookHit() {
+        if (isAlive) {
+            setPowerLevel(Math.max(powerLevel - 5, 0));
+        }
     }
 
     //================== Do Not Touch Code Below this line =============================
     public String toString(){
         return "Grok:("+powerLevel+","+isAlive+")";
+
     }
 }
